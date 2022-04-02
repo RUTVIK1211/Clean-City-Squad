29e2450b724a6ebe1e74bd2b33e7f0ee029eb585
ronakagnani42 <ronakagnani42@gmail.com>
2022-04-02 16:38:42 +0530

    complaints_model

---
+++ app/Http/Controllers/ComplaintsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ComplaintsController extends Controller
+{
+    public function getdata(){
+
+    }
+
+    public function newComplaint(){
+        
+    }
+}
--- database/migrations/2022_04_01_134845_create_complaints_table.php
+++ database/migrations/2022_04_01_134845_create_complaints_table.php
@@ -13,7 +13,8 @@ class CreateComplaintsTable extends Migration
      */
     public function up()
     {
-        Schema::create('complaints', function (Blueprint $table) {
+        Schema::create('complaints', function (Blueprint $table) 
+        {
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
@@ -22,9 +23,11 @@ class CreateComplaintsTable extends Migration
             $table->decimal('longitude', 10,8);
             $table->text('address_line_1');
             $table->text('address_line_2');
+            $table->text('area_id');
+            $table->text('complaint_type_id');
             $table->integer('status')->default('0');
 
-            $table->timestamps();
+            $table->timestamps()->nullable();
         });
     }
 
