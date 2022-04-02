7cfae46fb59f51422a6328987aba539a454961da
Rutvik <rutvik.b@simformsolutions.com>
2022-04-03 04:35:29 +0530

    fixed migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,10 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone_number');
+            $table->boolean('is_otp_verified')->default(0);
+            $table->unsignedBigInteger('area_id')->nullable();
+            $table->boolean('is_admin')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
